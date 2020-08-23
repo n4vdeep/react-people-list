2a4eaf3e62b52bdecf137d72df395a5b3931d6a2
Nav Dhamu <n4vdeep@gmail.com>
2020-08-23 19:39:01 +0100

    added styling for each person component

--- people_list/src/index.js
+++ people_list/src/index.js
@@ -2,19 +2,26 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-// DEFINE PERSON COMPONENT
-// DESTRUCTOR PROPS
-// CHILDREN PROPS
-const Person = ({ img, name, occupation, children }) => {
+// DEFINE PERSON COMPONENT &
+// DESTRUCTOR PROPS &
+// DEFINE CHILDREN PROPS
+const Person = props => {
+  // Destructor props
+  const { img, name, occupation } = props.personInfo;
+  // Put image url into a variable to make 
+  // dynamic selection of image
   const url = `https://randomuser.me/api/portraits/thumb/men/${img}.jpg`
 
+  //Children prop
+  const { children } = props;
+
   return (
-    <article>
+    <article className="person">
       <img
         src={url} alt="person"
       />
       <h4>{name}</h4>
-      <h4>{occupation}</h4>
+      <h5>{occupation}</h5>
       {children}
     </article>
   );
@@ -22,25 +29,56 @@ const Person = ({ img, name, occupation, children }) => {
 
 
 const PersonList = () => {
+  const personInfo = [
+    {
+      img: "34",
+      name: "John",
+      occupation: "Developer"
+    },
+    {
+      img: "22",
+      name: "Andrew",
+      occupation: "Designer"
+    },
+    {
+      img: "56",
+      name: "Dillon",
+      occupation: "Manager"
+    },
+    {
+      img: "54",
+      name: "Brandon",
+      occupation: "CEO"
+    },
+  ]
+
   return (
-    <section>
-      <Person img="34" name="John" occupation="developer" />
-      <Person img="22" name="Andrew" occupation="designer">
+    <section className="person__list">
+      <Person personInfo={personInfo[0]} >
+        <p>
+          Lorem ipsum dolor sit amet. Sit amet consectetur adipisicing elit. Recusandae, quis.
+        </p>
+      </Person>
+
+      <Person personInfo={personInfo[1]}>
         <p>
           Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aperiam, minima!
         </p>
       </Person>
-      <Person img="56" name="Dillon" occupation="manager" />
+
+      <Person personInfo={personInfo[2]}>
+        <p>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aperiam, minima!
+        </p>
+      </Person>
+
+      <Person personInfo={personInfo[3]}>
+        <p>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aperiam, minima!
+        </p>
+      </Person>
     </section>
   );
 }
-
-
-
-
-
-
-
-
 
 ReactDOM.render(<PersonList />, document.getElementById('root'));
